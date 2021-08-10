068cff4b3809f6195ae6ae7f3accb2a9056d4dcc
Gabriel Lazarov <75668721+gabriellazarov@users.noreply.github.com>
2021-08-10 23:40:04 +0300

    Add files via upload

--- REST API/index.js
+++ REST API/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const { PORT } = require('./config');
 const databaseConfig = require('./config/database');
@@ -6,15 +7,21 @@ const expressConfig = require('./config/express');
 const routesConfig = require('./config/routes');
 
 
+
 start();
 
 async function start() {
-    const app = express();
+  const app = express();
 
-    await databaseConfig(app);
-    expressConfig(app);
-    routesConfig(app);
+  app.use(cors({
+    origin: 'http://localhost:4200',
+    credentials: true
+  }));
+
+  await databaseConfig(app);
+  expressConfig(app);
+  routesConfig(app);
 
 
-    app.listen(PORT, () => console.log(`Application started at http://localhost:${PORT}`));
+  app.listen(PORT, () => console.log(`Application started at http://localhost:${PORT}`));
 }
